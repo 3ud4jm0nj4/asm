7e257aad3a9fa6341bfd608bd154a21cb0cf8460
3ud4jm0nj4 <tranduynam123@outlook.com>
2022-08-20 21:44:17 +0700

    asm

--- practicalRE/strchr.asm
+++ practicalRE/strchr.asm
@@ -1,32 +1,37 @@
 include \masm32\include\masm32rt.inc
 
 .data
- a db "bgt",0
- b db "g",0
+ a db "xinchao",0
+ b db "c",0
 .code
 
 strchr proc
 push ebp
 mov ebp, esp
-mov edi, [ebp+8]     ; get first parameter
-mov bl, [ebp+12]     ; set bl to second parameter
-mov al, 0            ; set al to null byte
+mov edi, [ebp+8]   ;lay ki tu dau tien cua chuoi
+mov esi,edi         ;luu vi tri dau tien cua chuoi
+mov bl, [ebp+12]     ;lay ki tu can so sanh "b"
+mov al, 0            
 _loop:
-mov cl, [edi]        ; store current character
-cmp cl, bl           ; check if character is what we search         
-jz _return           ; jump to return if match
-scasb                ; check if null byte
-jnz _loop            ; loop if no match
-mov edi, 0           ; set edi to zero, so function will return null
+mov cl, [edi]        
+cmp cl, bl                    
+jz _return           
+scasb                
+jnz _loop                      
 _return:
-mov eax, edi         ; return pointer to first occurence 
+add edi,1
+sub edi,esi
+print str$(edi)
+mov eax, edi          
 mov esp, ebp
 pop ebp
 ret
 strchr endp
 
 main:
-
+movzx eax,b ;chuyen ki tu vao thanh ghi movzx chuyen 8 bit vao 32 bit
+push eax    
+push offset a
 call strchr
 exit 
 
